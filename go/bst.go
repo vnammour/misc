f03188874785fa1486e240da51cf5d60ed407db1
Victor Nammour <vnammour@gmail.com>
2026-07-10 00:14:04 +0300

    bst

--- go/bst.go
+++ go/bst.go
@@ -12,36 +12,96 @@ type TreeNode struct {
 }
 
 func main() {
-	data := []int{5,4,7,8,2,1,5,6}
+	// data := []int{5,4,7,8,2,1,5,6}
 	// data := []int{4,1,7,5}
+	data := []int{15, 5, 16, 20, 18, 23, 3, 12, 13, 10, 6, 7}
 	var root *TreeNode
 	for _,val := range data {
 		root = Insert(root,val)
 	}
 	// InOrderTraversal(root)
-	inordertraversal(root)
-	fmt.Println("depth = ",Depth(root))
-	fmt.Println("depth = ", depth(root))
-
-	node := Search(root,1)
-	rplc := &TreeNode{Data: 3}
-	fmt.Println(node,rplc)
-	root = transplant(root, node, rplc)
-	inordertraversal(root)
+	// inordertraversal(root)
+	// fmt.Println("depth = ",Depth(root))
+	// node := Search(root, 5)
+	// fmt.Println(node)
+	// node = Delete(root, node)
+	// inordertraversal(root)
+	for key,value := range hashtree(root) {
+		fmt.Printf("%p = %d\n", value, key)
+	}
 }
 
-func TreeSuccessor(root *TreeNode) *TreeNode {
-	if root == nil || root.Right == nil {
-		return root
+func hashtree(root *TreeNode) map[int]*TreeNode {
+	if root == nil {
+		return nil
 	}
-	return TreeMin(root.Right)
+	sz := pow2(Depth(root)) - 1
+	hash := make(map[int]*TreeNode, sz)
+	temp := root
+	dummy := &TreeNode{Data:root.Data} // so that root will overwrite dummy before prcedure ends.
+	stack := append(make([]*TreeNode, 0, sz), dummy)
+	for len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		// if root != dummy {
+		hash[root.Data] = root
+		// }
+		root = root.Right
+	}
+	hash[temp.Data] = temp
+	return hash
 }
 
-func TreePredecessor(root *TreeNode) *TreeNode {
-	if root == nil || root.Left == nil {
+// The procedure runs in O(h) time on a tree of height h.
+func Delete(root, z *TreeNode) *TreeNode {
+	if root == nil || z == nil {
 		return root
 	}
-	return TreeMax(root.Left)
+	// if z has no children, we modify it's parent to replace z with null as its child.
+	var x,y *TreeNode
+	// Determining a node y to splice out. y is either the input node z (if z has at most
+	// one child) or the successor of z (if z has two children).
+	if z.Left == nil || z.Right == nil {
+		y = z
+	} else {
+		y = TreeSuccessor(z)
+	}
+	// x is set to the non-nil child of y, or to null if y has no children.
+	if y.Left != nil {
+		x = y.Left
+	} else {
+		x = y.Right
+	}
+	// Node y is spliced out by modifying its pointers in y.Parent and x.
+	// Splicing out y is somewhat complicated by the need for proper handling of the boundary
+	// conditions, which occur when x = nil or when y is the root.
+	if x != nil {
+		x.Parent = y.Parent
+	}
+	if y.Parent == nil {
+		root = x
+	} else {
+		if y == y.Parent.Left {
+			y.Parent.Left = x
+		} else {
+			y.Parent.Right = x
+		}
+	}
+	
+	// If the successor of z was the node spliced out, y's key and satellite data are moved to z,
+	// overwriting the previous key and satellite data.
+	if y != z {
+		z.Data = y.Data
+	}
+	// VJN
+	// removing all reference from y to other nodes in the tree.
+	y.Parent, y.Left, y.Right = nil, nil, nil
+	// The node y is returned so that the calling procedure can recycle it via the free list.
+	return y
 }
 
 func TreeMin(root *TreeNode) *TreeNode {
@@ -58,6 +118,37 @@ func TreeMax(root *TreeNode) *TreeNode {
 	for x != nil {
 		y = x
 		x = x.Right
+	}
+	return y
+}
+
+
+func TreeSuccessor(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Right != nil {
+		return TreeMin(root.Right)
+	}
+	var x, y = root, root.Parent
+	for y != nil && x == y.Right {
+		x = y
+		y = y.Parent
+	}
+	return y
+}
+
+func TreePredecessor(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Left != nil {
+		return TreeMax(root.Left)
+	}
+	var x,y *TreeNode = root, root.Parent
+	for y != nil && x == y.Left {
+		x = y
+		y = y.Parent
 	}
 	return y
 }
@@ -126,7 +217,7 @@ func inordertraversal(root *TreeNode) {
 	}
 }
 
-func transplant(root, node, rplc *TreeNode) *TreeNode {
+func Transplant(root, node, rplc *TreeNode) *TreeNode {
 	if node == nil {
 		fmt.Println("0")
 		return root
@@ -176,31 +267,11 @@ func Depth(root *TreeNode) int {
 	}
 }
 
-func depth(root *TreeNode) int {
-	if root == nil {
-		return 0
+func pow2(n int) int {
+	num := 1
+	for n > 0 {
+		num *= 2
+		n--
 	}
-	queue := append(make([]*TreeNode,0,10),root)
-	count := 1
-	bfs_depth(queue,&count)
-	return count
-}
-
-func bfs_depth(queue []*TreeNode,count *int) {
-	if len(queue) > 0 {
-		node := queue[0]
-		queue = queue[1:]
-		if node != nil {
-			queue = append(queue,node.Left,node.Right)
-			*count++
-			if node.Left != nil {
-				fmt.Printf("enqueuing %d\n", node.Left.Data)
-			}
-			if node.Right != nil {
-				fmt.Printf("enqueuing %d\n", node.Right.Data)
-			}
-			fmt.Printf("count = %d\n", *count)
-		}
-		bfs_depth(queue,count)
-	}
+	return num
 }
