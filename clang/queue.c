6d8637ff27136292468fe9ac27262a338f607719
Victor Nammour <vnammour@gmail.com>
2026-07-14 23:37:07 +0300

    queue

--- clang/queue.c
+++ clang/queue.c
@@ -27,47 +27,88 @@ Queue init(size_t cap) {
   };
 }
 
+Queue *new(size_t cap) {
+    err = 0;
+    Queue *q = (Queue*) malloc(sizeof(Queue));
+    if (q == NULL) {
+        err = 3;
+        return q;
+    }
+    q->buf = (int*) malloc(sizeof(int) * cap);
+    if (q->buf == NULL) {
+        err = 3;
+        return NULL;
+    }
+    q->cap = cap;
+    q->h = q->t = q->buf;
+    return q;
+}
+
+char * errstring(int err) {
+    if (err < 0) {
+        return "Unknown error.";
+    }
+    switch (err) {
+    case 0: return "none";
+    case 1: return "overflow";
+    case 2: return "underflow";
+    case 3: return "no memory";
+    default: return "other";
+    }
+}
+
 void enqueue(Queue *q, int data) {
   err = 0;
-  if (q == NULL) err = 4;
+  if (q == NULL) {
+      err = 4;
+      return;
+  }
   int *h = q->h, *t = q->t, *buf = q->buf;
   int cap = q->cap;
   int index = (t - buf + 1) % cap;
   if (h - buf == index) { // queue is full, i.e. overflow
     err = 1;
-    return
+    return;
   }
-  if (index == 0) t = buf;
-  *t++ = data;
+  *q->t++ = data;
+  if (index == 0) q->t = buf;
 }
 
-int dequeue(Queue *q) {}
+int dequeue(Queue *q) {
+  if (q == NULL) {
+      err = 4;
+      return 0;
+  }
+    err = 0;
+    if (q->h == q->t) {
+        err = 2;
+        return 0;
+    }
+    return *q->h++;
+}
 
 int length(Queue *q) {
-  return q->h == q->t ? 0 : q->buf + q->cap - q->h + q->t - q->buf;
+  return q->t > q->h ? q->t - q->h : q->buf + q->cap - q->h + q->t - q->buf;
 }
 
-/*void enqueue(Queue *q, int buf) {
-  if (q->length == q->cap) {
-    int cap = q->length;
-    if (q->length < 2 * q->cap) {
-      cap = 2 * q->cap;
+size_t capacity(Queue *q) {
+    return q->cap - 1;
+}
+
+int main(int argc, char *argv[]) {
+    // err = 0;
+    printf("main: err = %d\n", err);
+    // Queue q = init(3); // & use (&q)
+    Queue *q = new(5);
+    for (int i = 0; i < capacity(q); i++) {
+        enqueue(q,i);
+        if (err != 0) printf("enqueue: %s - err = %d\n", errstring(err), err);
     }
-    ptrdiff_t dh = q->h - q->buf;
-    ptrdiff_t dt = q->t - q->buf;
-    int *temp = realloc(q->buf, cap);
-    if (temp == NULL) {
+    printf("length = %d\n", length(q));
+    int temp;
+    for (int i = 0; i < capacity(q); i++) {
+        temp = dequeue(q);
+        if (err != 0) printf("dequeue: %s - err = %d\n", errstring(err), err);
+        else printf("%d\n", temp);
     }
-    q->buf = temp;
-    q->h = q->buf + dh;
-    q->t = q->buf + dt;
-    q->cap = cap;
-  }
-  int index = ++q->length % q->cap;
-  q->t[index] = buf;
-  if (index == 0)
-    q->t = q->buf;
-  else
-    ++q->t;
-}*/
-int main(int argc, char *argv[]) {}
+}
